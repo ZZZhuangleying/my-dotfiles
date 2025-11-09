a21aa781aac597259e08dc1110b2f5b6302f6972
zly <zhuangleying24@mails.ucas.ac.cn>
2025-11-09 22:47:46 +0800

    initial install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 更新包列表并升级现有软件
+sudo apt update && sudo apt upgrade -y
+
+# 安装基础工具
+sudo apt install -y git curl vim tmux
+
+# 克隆 Dotfiles 仓库
+git clone https://github.com/ZZZhuangleying/my-dotfiles ~/my-dotfiles
+
+# 创建符号链接
+ln -sf ~/my-dotfiles/.bashrc ~/
+ln -sf ~/my-dotfiles/.vimrc ~/
+ln -sf ~/my-dotfiles/.tmux.conf ~/
+
+echo "Setup complete! Please restart your terminal."
