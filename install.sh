2718c00f9594a0e61ba738e796c93660d2742013
zly <zhuangleying24@mails.ucas.ac.cn>
2025-12-15 22:05:16 +0800

    delete origin files in install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # 更新包列表并升级现有软件
-sudo apt update && sudo apt upgrade -y
+# sudo apt update && sudo apt upgrade -y
 
 # 安装基础工具
-sudo apt install -y git curl vim tmux
+# sudo apt install -y git curl vim tmux
 
 # 克隆 Dotfiles 仓库
-git clone https://github.com/ZZZhuangleying/my-dotfiles ~/my-dotfiles
+# git clone https://github.com/ZZZhuangleying/my-dotfiles ~/my-dotfiles
+
+# 删除原文件
+rm -f ~/.bashrc
+rm -f ~/.vimrc
+rm -f ~/.tmux.conf
 
 # 创建符号链接
 ln -sf ~/my-dotfiles/.bashrc ~/
